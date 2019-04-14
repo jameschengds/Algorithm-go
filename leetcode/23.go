1dbfa1f63e5cdb9c50f27a5fcb216de83586b6d0
chengds <jameschengds@gmail.com>
2019-04-15 00:12:48 +0800

    24. Swap Nodes in Pairs

---
+++ leetcode/23.go
@@ -0,0 +1,18 @@
+package leetcode
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func swapPairs(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	next := head.Next
+	head.Next = swapPairs(next.Next)
+	next.Next = head
+	return next
+}
